070d3895a4b417c230ac39eedd49bef7c8598a5e
admeng.liu <admeng.liu@qunar.com>
2018-08-27 08:53:30 +0800

    init

---
+++ app/src/main/cpp/dalvik/cutils/log.h
@@ -0,0 +1,13 @@
+//
+// Created by liu meng on 2018/8/27.
+//
+
+#ifndef CUSTOMDALVIK_LOG_H
+#define CUSTOMDALVIK_LOG_H
+#define ALOGV(a,...)
+#define ALOGW(a,...)
+#define ALOGE(a,...)
+#define ALOGI(a,...)
+#define ALOGD(a,...)
+#define LOG_VFY(a,...)
+#endif //CUSTOMDALVIK_LOG_H
